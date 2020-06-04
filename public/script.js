d7aa4e4fdccad2a675c843335116d4714f7d6208
barbara-mooney <60058920+barbara-mooney@users.noreply.github.com>
2020-06-03 17:49:28 -0700

    made changes to the portfolio page

--- public/script.js
+++ public/script.js
@@ -1,26 +1,3 @@
-// function scrollToProjects () {
-//   window.scrollTo({
-//     top: 750,
-//     bottom: 0, 
-//     behavior: 'smooth'
-//   });
-
-// projects.addEventListener('click', function() {
-//   window.scrollTo({
-//     top: 750,
-//     bottom: 0, 
-//     behavior: 'smooth'
-//   });
-// });
-
-// $(document).ready(function() {
-//   var url = window.location;
-//   var alertStatus = new URLSearchParams(url.search).get('alert');
-//   if (alertStatus) {
-//     alert('Thank you for your message. I will get back to you soon!');
-//   }
-// });
-
 var slideIndex = 0;
 showSlides();
 
@@ -33,5 +10,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
 }
